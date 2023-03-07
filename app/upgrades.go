864f9686509cd4681ca4ba84799d343a66de27ad
Sai Kumar <sai@umee.cc>
2023-03-07 17:23:15 +0530

    chore: removed v4.2 upgrade handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -17,7 +17,6 @@ import (
 	leveragetypes "github.com/umee-network/umee/v4/x/leverage/types"
 	oraclekeeper "github.com/umee-network/umee/v4/x/oracle/keeper"
 	oracletypes "github.com/umee-network/umee/v4/x/oracle/types"
-	"github.com/umee-network/umee/v4/x/uibc"
 )
 
 // RegisterUpgradeHandlersregisters upgrade handlers.
@@ -35,20 +34,20 @@ func (app UmeeApp) RegisterUpgradeHandlers(bool) {
 	app.registerUpgrade3_3to4_0(upgradeInfo)
 	app.registerUpgrade4_0_1(upgradeInfo)
 	app.registerUpgrade4_1(upgradeInfo)
-	app.registerUpgrade4_2(upgradeInfo)
+	// app.registerUpgrade4_2(upgradeInfo)
 }
 
-// performs upgrade from v4.1 to v4.2
-func (app *UmeeApp) registerUpgrade4_2(upgradeInfo upgradetypes.Plan) {
-	const planName = "v4.2"
-	app.UpgradeKeeper.SetUpgradeHandler(planName, onlyModuleMigrations(app, planName))
+// // performs upgrade from v4.1 to v4.2
+// func (app *UmeeApp) registerUpgrade4_2(upgradeInfo upgradetypes.Plan) {
+// 	const planName = "v4.2"
+// 	app.UpgradeKeeper.SetUpgradeHandler(planName, onlyModuleMigrations(app, planName))
 
-	app.storeUpgrade(planName, upgradeInfo, storetypes.StoreUpgrades{
-		Added: []string{
-			uibc.ModuleName,
-		},
-	})
-}
+// 	app.storeUpgrade(planName, upgradeInfo, storetypes.StoreUpgrades{
+// 		Added: []string{
+// 			uibc.ModuleName,
+// 		},
+// 	})
+// }
 
 // performs upgrade from v4.0 to v4.1
 func (app *UmeeApp) registerUpgrade4_1(_ upgradetypes.Plan) {
